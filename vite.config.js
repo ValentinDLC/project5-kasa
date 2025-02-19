4486a40cee3d31e7252156bad26224cfcd15bf70
ValentinDLC <70979146+ValentinDLC@users.noreply.github.com>
2025-02-19 15:50:53 +0100

    configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/project5-kasa/',
   optimizeDeps: {
     include: ["react", "react-dom"],
   },
