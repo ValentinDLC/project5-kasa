c66e1fba0d7ab8ef6fdb9a9fab31cec59a337396
ValentinDLC <70979146+ValentinDLC@users.noreply.github.com>
2025-02-19 16:28:26 +0100

    essaie1

--- src/Data/BannerData.js
+++ src/Data/BannerData.js
@@ -2,11 +2,11 @@ import "../assets/sass/Components/Banner.scss";
 export const homeBanner = {
   title: "Chez vous, partout et ailleurs",
   alt: "Bannière Kasa",
-  imageUrl: "/homebanner(1).png",
+  imageUrl: `${process.env.PUBLIC_URL}/homebanner(1).png`,
 };
 
 export const aboutBanner = {
   title: "",
   alt: "Bannière À propos",
-  imageUrl: "/AboutBanner.png",
+  imageUrl: `${process.env.PUBLIC_URL}/AboutBanner.png`,
 };
